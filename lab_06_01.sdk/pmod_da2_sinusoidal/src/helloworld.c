89b33c9df8f6209aade07fe0544dc69d385aa1cd
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2020-10-21 03:42:26 -0400

    Tested Discovery board with PmodDA2Test.c. Added definitions.

--- lab_06_01.sdk/pmod_da2_sinusoidal/src/helloworld.c
+++ lab_06_01.sdk/pmod_da2_sinusoidal/src/helloworld.c
@@ -45,17 +45,50 @@
  *   ps7_uart    115200 (configured by bootrom/bsp)
  */
 
-#include <stdio.h>
-#include "platform.h"
+/* Xilinx includes. */
 #include "xil_printf.h"
+#include "xparameters.h"
+#include "xgpio.h"
+#include "xstatus.h"
+
+/* platform and library includes */
+#include "platform.h"
 #include <math.h>
 
+//DAC2Pmod from Address Editor in Vivado
+#define DA2acq  0x43C00000   //DA2 acquisition    - output
+#define DA2dav	0x43C00004	 //DA2 data available - input
+#define DA2dat1	0x43C00008	 //DA2 channel 1 data - output
+#define DA2dat2 0x43C0000C	 //DA2 channel 2 data - output
+
+/* GPIO definitions */
+#define INP_GPIO_DEVICE_ID  XPAR_AXI_GPIO_0_DEVICE_ID	/* GPIO device for input */
+#define FRQ_CHANNEL 1								/* GPIO port for buttons for frequency */
+#define AMP_CHANNEL 2								/* GPIO port for switches for amplitude */
+/*-----------------------------------------------------------*/
+
+#define DA2bits	12
 
 int main()
 {
-    init_platform();
+	XGpio InpInst;	/* GPIO Device driver instance for the input (buttons, switches) */
 
-    print("Hello World\n\r");
+	int max_p1_sin = 1 << 12;		/* max sinusoid + 1 */
+	int max_amp = max_p1_sin >> 1;	/* the max amplitude and DC offset */
+
+	xil_printf("Initializing GPIO for input . . .\n");
+	/* GPIO driver initialization */
+	if (XGpio_Initialize(&InpInst, INP_GPIO_DEVICE_ID) != XST_SUCCESS) {
+		return XST_FAILURE;
+	}
+
+	/* set direction of both channels to output */
+	XGpio_SetDataDirection(&InpInst, FRQ_CHANNEL, 0xFF);
+	XGpio_SetDataDirection(&InpInst, AMP_CHANNEL, 0xFF);
+
+	init_platform();
+
+    xil_printf("Hello World\n\r");
 
     cleanup_platform();
     return 0;
